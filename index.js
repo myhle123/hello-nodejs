0510a7e0024fde963f5e2ca41521e505c43d9d4c
myhle123 <135527079+myhle123@users.noreply.github.com>
2024-04-16 22:39:38 +0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-app.get('/contact', function (req, res) {
-    res.send('Hello World!!!')
-});
-app.listen(3000, function () {
-    console.log('Example 1 app listening on port 3000!');
-
-});
